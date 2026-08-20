341d98646c3dbe52802af07cd2a88063072f464e
marianofalu <marianofalu@gmail.com>
2026-08-20 19:42:51 -0300

    add: header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <header>
+            <div className="logo">
+                <img src="/assets/img/logo.png" alt="logo" />
+                <h1>Character Counter</h1>
+            </div>
+
+            <button>⚙️</button>
+        </header>
+    )
+}
+
+export { Header }

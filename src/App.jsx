a4bbfc26b74691d3de93fa8bfc5066dd1da09ccd
marianofalu <marianofalu@gmail.com>
2026-08-20 19:52:35 -0300

    add: writearea component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { Header } from "./components/Header.jsx"
+import { WriteArea } from "./components/WriteArea.jsx"
 
 function App() {
     return (
-        <main>
-            <Header />
-        </main>
+      <main>
+          <Header />
+
+          <h2>Analyze your text <br />
+              in real-time.
+          </h2>
+
+          <WriteArea />
+      </main>
     )
 }
 
---
+++ src/components/WriteArea.jsx
@@ -0,0 +1,10 @@
+const WriteArea = () => {
+    return (
+        <textarea
+            className="text-area"
+            placeholder="Start typing here..."
+        ></textarea>
+    )
+}
+
+export { WriteArea }
